6f6bf7d7060fa375a5f311067ff0648fc444c62c
insolor <insolor@gmail.com>
2022-06-15 16:04:28 +0300

    Add an automation script to generate a json file with files grouped by language

---
+++ automation/automation/files_by_language.py
@@ -0,0 +1,29 @@
+from collections import defaultdict
+import typer
+import json
+from pathlib import Path, PurePosixPath
+
+
+app = typer.Typer()
+
+
+@app.command()
+def group_files_by_language(translations_directory: Path):
+    result = defaultdict(dict)
+    """
+    {
+        "ru": {
+            "hardcoded": "dwarf-fortress.hardcoded/ru.po",
+            "raw-objects": "dwarf-fortress.hardcoded/ru.po",
+            ...
+        }
+    }
+    """
+
+    for directory in translations_directory.glob("*"):
+        resource_name = directory.name.partition(".")[2]
+        for file in directory.glob("*.po"):
+            language = file.stem
+            result[language][resource_name] = str(PurePosixPath(directory.name) / file.name)
+    
+    print(json.dumps(result, indent=4))
