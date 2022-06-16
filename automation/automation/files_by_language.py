0d3c9b944985b489583a8804c407a4661f38cd43
insolor <insolor@gmail.com>
2022-06-16 08:00:54 +0300

    Add assert on empty resource name, add some comments

--- automation/automation/files_by_language.py
+++ automation/automation/files_by_language.py
@@ -21,9 +21,10 @@ def group_files_by_language(translations_directory: Path):
     """
 
     for directory in translations_directory.glob("*"):
-        resource_name = directory.name.partition(".")[2]
+        resource_name = directory.name.partition(".")[2]  # "dwarf-fortress.hardcoded" -> "hardcoded"
+        assert resource_name, f"Unable to extract a resource name (no dot in the directory name or no text after dot): {directory.name:!r}"
         for file in directory.glob("*.po"):
-            language = file.stem
+            language = file.stem  # Path("dwarf-fortress.hardcoded/ru.po") -> "ru"
             result[language][resource_name] = str(PurePosixPath(directory.name) / file.name)
     
     assert result, "Empty result"
